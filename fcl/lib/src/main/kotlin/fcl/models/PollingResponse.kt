135b271d4269ac670162d027a53f16221abd24e2
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2021-09-25 18:47:30 +0200

    api models

---
+++ fcl/lib/src/main/kotlin/fcl/models/PollingResponse.kt
@@ -0,0 +1,18 @@
+package fcl.models
+
+data class PollingResponse (
+    val status: String,
+    val data: PollingData?,
+    val updates: Service?
+)
+
+data class PollingData (
+    val addr: String,
+    val services: Array<Service>?
+)
+
+data class Service(
+    val type: String,
+    val method: String,
+    val endpoint: String
+)
